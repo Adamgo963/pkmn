be78cd5bb264b49e0622e6ac7d32bb07efd0b0dd
Adam Gonda <433421@mail.muni.cz>
2021-02-23 22:41:35 +0100

    Added authentication, appbar, some styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,54 @@ import './App.css';
 import SetList from './components/SetList';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import CardList from './components/CardList';
+import React, { FC, useEffect } from 'react';
+import { signIn, useUser } from './utils/firebase';
+import { createMuiTheme, makeStyles, MuiThemeProvider, Theme } from '@material-ui/core/styles';
+import { blue, indigo } from '@material-ui/core/colors';
+import { AppBar, Button, Grid, Toolbar } from '@material-ui/core';
 
-function App() {
+const useStyles = makeStyles((theme: Theme) => ({
+  container: {
+    margin: 20
+  }
+}));
+
+const appTheme = createMuiTheme({ 
+  palette: {
+      primary: indigo,
+      secondary: blue,
+      }, 
+});
+
+const App: FC = () => {
   
+  useEffect(() => {
+    signIn("adamgo963@gmail.com", "test963")
+  }, []);
+
+  const user = useUser();
+  const classes = useStyles();
+  
+  console.log(user);
+
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact render={() => { return <SetList />}}/>
-        <Route path="/sets/:setCode" render={({ match }) => <CardList setCode={match.params.setCode}/>}/>
-      </Switch>
-    </Router>
+    <MuiThemeProvider theme={appTheme}>
+      <Router>
+        <AppBar color="primary" position="static" variant="outlined">
+          <Toolbar>
+            <h1>Pokémon TCG App</h1>
+          </Toolbar>
+        </AppBar>
+        <Grid container className={classes.container}>
+          <Switch>
+            <Route path="/" exact render={() => { return <SetList />}}/>
+            <Route path="/sets/:setCode" render={({ match }) => <CardList setCode={match.params.setCode}/>}/>
+          </Switch>
+        </Grid>
+      </Router>
+    </MuiThemeProvider>
+
+    
   )  
 }
 
---
+++ src/utils/firebase.ts
@@ -0,0 +1,34 @@
+import firebase from 'firebase/app';
+import 'firebase/auth'
+import { useEffect, useState } from 'react';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyB4R1Q0CmwMR3IPmw0hx2P9-kcoXZkYf2E",
+  authDomain: "pkmn-app.firebaseapp.com",
+  projectId: "pkmn-app",
+  storageBucket: "pkmn-app.appspot.com",
+  messagingSenderId: "259672784200",
+  appId: "1:259672784200:web:8c0589736d2224d5ffc77d"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export const signIn = (email: string, password: string) => {
+  firebase.auth().signInWithEmailAndPassword(email, password)
+    .catch((e) => {
+      console.log('Sing In Error', e)
+    })
+}
+
+export const useUser = () => {
+  
+  const [user, setUser] = useState<firebase.User | null>()
+  
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(u => setUser(u))
+  }, [])
+  
+  return user;
+}
